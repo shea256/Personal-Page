fcb3dbdaf3912b519ccc973b3287b9067748d2a6
Ryan Shea <me@ryanshea.org>
2012-03-17 05:14:51 -0400

    set up portfolio and bio pages

--- homebase.py
+++ homebase.py
@@ -1,14 +1,37 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template, send_from_directory, request
 import os
+import base64
+
 app = Flask(__name__)
+
+app.config.update(
+    DEBUG = False,
+)
 
 @app.route('/')
 def aboutme():
+    phone_number = request.args.get('phone_number', '')
+    s1 = request.args.get('s1', '')
+    email = request.args.get('email', '')
+    s2 = request.args.get('s2', '')
+    
+    iv = request.args.get('v1', '')
+    salt = request.args.get('v2', '')
+    ct = request.args.get('v3', '')
+    
+    n = request.args.get('n', '')
+    decoded_name = base64.b64decode(n)
+    print decoded_name + " just visited your about page"
+    
     return render_template('aboutme.html')
 
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
+
+@app.route('/bio')
+def bio():
+    return render_template('bio.html')
 
 @app.route('/favicon.ico')
 def favicon():
