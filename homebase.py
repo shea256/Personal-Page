21c6fc7870c057c2945590189e7f7ad4e0aa0e98
Ryan Shea <me@ryanshea.org>
2012-03-18 23:07:43 -0400

    ported crypto and hash functions to python code

--- homebase.py
+++ homebase.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_from_directory, request
 import os
 import base64
 from Crypto.Cipher import AES
+from Crypto.Hash import SHA
 import string
 import re
 
@@ -13,16 +14,39 @@ app.config.update(
 
 @app.route('/')
 def aboutme():
-    phone_number = request.args.get('phone_number', '')
+    #pn_salt = 'd786dff79a90'
+    #email_salt = 'b2832fa81995'
+    correct_phone_hash = 'YKHeeo7VOTo/OTI9Q+fPy/UUirw='
+    email_hash1 = 'UYTI6hPxlbUROrPG8L6DRh2gvdo='
+    email_hash2 = 'eWnrgcZ7f6h7dRjgCm1DjJRPQQY='
+    email_hash3 = 'SYk+FeQ18NpWPwve4tK8C/0gAfE='
+    phone_number = ""
+    email = ""
+    
+    # grab
+    unverified_phone_number = request.args.get('phone_number', '')
     s1 = request.args.get('s1', '')
-    email = request.args.get('email', '')
+    unverified_email = request.args.get('email', '')
     s2 = request.args.get('s2', '')
     
-    # capturing encrypted and base64-encoded parameter
-    m = request.args.get('m', '')
+    # verifying phone number
+    hash1 = SHA.new()
+    hash1.update(str(s1) + str(unverified_phone_number))
+    if (base64.b64encode(hash1.digest()) == correct_phone_hash):
+        phone_number = unverified_phone_number
     
+    # verifying email address
+    hash2 = SHA.new()
+    hash2.update(str(s2) + str(unverified_email))
+    b = base64.b64encode(hash2.digest())
+    if (b == email_hash1 or b == email_hash2 or b == email_hash3):
+        email = unverified_email
+    
+    # capture encrypted and base64-encoded parameter
+    # if no viewer tracking id is present, stop and render the template
+    m = request.args.get('m', '')
     if len(m) < 1:
-        return render_template('aboutme.html')
+        return render_template('aboutme.html', email=email, phone_number=phone_number)
     
     # base64 decoding parameter
     try:
@@ -47,7 +71,7 @@ def aboutme():
     else:
         print "invalid URL attempted"
     
-    return render_template('aboutme.html')
+    return render_template('aboutme.html', phone_number=verified_phone_number, email=verified_email)
 
 @app.route('/encryptname/<name>')
 def encryptname(name):
