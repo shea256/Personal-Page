5745e1ec397eaabe0c1f6625132452bf6d57d8ac
Ryan Shea <me@ryanshea.org>
2012-03-18 20:27:13 -0400

    adding crypto library as well as encryption and decrption for about page

--- homebase.py
+++ homebase.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template, send_from_directory, request
 import os
 import base64
+from Crypto.Cipher import AES
+import string
+import re
 
 app = Flask(__name__)
 
 app.config.update(
-    DEBUG = False,
+    DEBUG = True,
 )
 
 @app.route('/')
@@ -15,15 +18,55 @@ def aboutme():
     email = request.args.get('email', '')
     s2 = request.args.get('s2', '')
     
-    iv = request.args.get('v1', '')
-    salt = request.args.get('v2', '')
-    ct = request.args.get('v3', '')
+    # capturing encrypted and base64-encoded parameter
+    m = request.args.get('m', '')
     
-    n = request.args.get('n', '')
-    decoded_name = base64.b64decode(n)
-    print decoded_name + " just visited your about page"
+    if len(m) < 1:
+        return render_template('aboutme.html')
+    
+    # base64 decoding parameter
+    try:
+        m = base64.urlsafe_b64decode(str(m))
+    except:
+        print "base64 decoding... invalid URL attempted"
+        return render_template('aboutme.html')        
+    
+    # ensuring parameter is 16 characters for decryption
+    if len(m) != 16:
+        print "checking length of string... invalid URL attempted"
+        return render_template('aboutme.html')
+    
+    # decrypting parameter
+    obj2 = AES.new('ball so hard808s', AES.MODE_ECB)
+    decoded_name = obj2.decrypt(m)
+
+    # stripping parameter of whitespace
+    decoded_name = string.rstrip(decoded_name)
+    if re.match("^[A-Z]'?[- a-zA-Z]+$", decoded_name):
+        print decoded_name + " just visited your about page"
+    else:
+        print "invalid URL attempted"
     
     return render_template('aboutme.html')
+
+@app.route('/encryptname/<name>')
+def encryptname(name):
+    
+    if not re.match("^[A-Z]'?[- a-zA-Z]+$", name):
+        return "<p>Invalid characters in name</p>"
+    
+    name = name[0:16]
+    name = string.ljust(name, 16)
+    
+    if len(name) != 16:
+        return "<p>Invalid length of string<p>"
+    
+    obj = AES.new('ball so hard808s', AES.MODE_ECB)
+    
+    ciphertext = obj.encrypt(name)
+    encoded64 = base64.urlsafe_b64encode(ciphertext)
+    
+    return "<p>Encoded name: " + encoded64 + "</p>"
 
 @app.route('/portfolio')
 def portfolio():
